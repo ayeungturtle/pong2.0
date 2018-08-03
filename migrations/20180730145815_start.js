4631ab51bc21ec4f1b5dd8d4a32357f19c178433
ayeungturtle <andrewbryantyeung@gmail.com>
2018-08-03 16:21:59 -0400

    ids consistently unsigned

--- migrations/20180730145815_start.js
+++ migrations/20180730145815_start.js
@@ -2,16 +2,16 @@
 exports.up = function(knex, Promise) {
     return knex.schema
     .createTable('lut_roles', function(r) {
-        r.integer('id').primary();
+        r.integer('id').primary().unsigned();
         r.string('role', 50).notNull();
         r.string('notes', 150).nullable().defaultTo(null);
     })
     .createTable('lut_game_modes', function(lgm) {
-        lgm.integer('id').primary();
+        lgm.integer('id').primary().unsigned();
         lgm.string('gameMode', 50).notNull();
     })
     .createTable('lut_achievement_types', function(lat) {
-        lat.integer('id').primary();
+        lat.integer('id').primary().unsigned();
         lat.string('achievementType', 50).notNull();
         lat.string('notes', 100).nullable().defaultTo(null);
     })   
@@ -20,7 +20,7 @@ exports.up = function(knex, Promise) {
         p.string('firstName', 50).notNull();
         p.string('lastName', 50).notNull();
         p.string('nickName', 50).nullable().defaultTo(null);
-        p.integer('lutRole').notNull();
+        p.integer('lutRole').unsigned().notNull();
         p.dateTime('dateTimeCreated', 6).nullable().defaultTo(knex.fn.now(6));
         p.foreign('lutRole').references('id').inTable('lut_roles').onDelete('restrict').onUpdate('cascade');
     })
@@ -30,7 +30,7 @@ exports.up = function(knex, Promise) {
         g.integer('loserId').unsigned().notNull();;
         g.integer('winnerScore').nullable().defaultTo(null);
         g.integer('loserScore').nullable().defaultTo(null);
-        g.integer('lutGameMode').nullable().defaultTo(null);
+        g.integer('lutGameMode').unsigned().nullable().defaultTo(null);
         g.dateTime('dateTime', 6).nullable().defaultTo(knex.fn.now(6));
         g.string('notes', 50).nullable().defaultTo(null);
         g.foreign('winnerId').references('id').inTable('players').onDelete('restrict').onUpdate('cascade');
@@ -40,7 +40,7 @@ exports.up = function(knex, Promise) {
     .createTable('achievements', function(a) {
         a.increments('id').primary();
         a.integer('playerId').unsigned().nullable().defaultTo(null);
-        a.integer('lutAchievementTypeId').index().nullable().defaultTo(null);
+        a.integer('lutAchievementTypeId').unsigned().nullable().defaultTo(null);
         a.integer('victimId').unsigned().nullable().defaultTo(null);
         a.integer('gameId').unsigned().nullable().defaultTo(null);
         a.dateTime('dateTime', 6).nullable().defaultTo(knex.fn.now(6));
